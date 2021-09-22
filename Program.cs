efeed032343197f0e28398e97c9209c53bb5fc8b
BBernYY <66414852+BBernYY@users.noreply.github.com>
2021-09-22 19:32:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var leeftijden = new Dictionary<string, int>(){
+                {"sytze", 11}, {"louise", 7}
+            };
+            string[] mensen = new string[]{"louise", "sytze"};
+            while (true) {
+                Random random = new Random();
+                var mens = mensen[random.Next(2)];
+                Console.WriteLine(mens);
+                var ans = Console.ReadLine();
+                Console.WriteLine(leeftijden[mens] == Int32.Parse(ans));
+                };
+        }
+    }
+}
